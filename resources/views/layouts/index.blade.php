48de10363e3efeb8ad757fc5014eb7d3307056bb
Sabit Huraira <sabitzhabit@gmail.com>
2021-02-21 11:57:57 +0700

    fixing layouts index

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -14,6 +14,7 @@
   <link rel="stylesheet" href="{{ asset('theme/adminlte') }}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('theme/adminlte') }}/dist/css/adminlte.min.css">
+  <!-- tambahan code untk css -->
   @yield('css')
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
@@ -307,6 +308,7 @@
 <script src="{{ asset('theme/adminlte') }}/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('theme/adminlte') }}/dist/js/demo.js"></script>
+<!-- tambahan code utk js -->
 @yield('script')
 </body>
 </html>
